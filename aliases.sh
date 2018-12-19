8e8553add51277376d0789ca4f35dc5e13e91b9e
Tony Yunker <tony.yunker@gmail.com>
2018-12-18 21:11:09 -0600

    new git aliases, refactor other aliases to aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,36 @@
+#general
+ alias vimrc='vim ~/.vimrc'
+alias bashrc='vim ~/.bashrc'
+alias cls='clear'
+alias reshell='source ~/.bashrc'
+
+# git
+alias gs='git status'
+
+alias gl='git log --oneline -10'
+alias gll='git log --oneline -50'
+
+# rails
+alias rs='rails server'
+alias rc='rails console'
+
+# RSpec
+alias rsm='rspec spec/models'
+alias rsc='rspec spec/controllers'
+alias rsf='rspec spec/features'
+alias rsl='rspec spec/lib'
+alias rsh='rspec spec/helpers'
+alias rsj='rspec spec/jobs'
+alias rsu='rspec --tag ~type:feature' 
+
+# MP aliases
+alias mpa='cd ~/code/machineryPete/mp_admin'
+alias mpd='cd ~/code/machineryPete/mp_dotcom'
+
+# FUN
+alias 'wttr'='curl -4 http://wttr.in/Chicago'
+alias 'wttrwork'='curl -4 https://wttr.in/~300%20S%20Riverside%20Plaza%20chicago'
+alias 'moon'='curl -4 http://wttr.in/moon'
+
+# Homebrew
+alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
