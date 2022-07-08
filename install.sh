eb13d229290ad0c22c68cfb9aa62b9da9e6bafe7
Tony Yunker <tony.yunker@gmail.com>
2022-07-08 08:55:52 -0500

    update installer

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 ln -sf ~/src/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/src/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/src/dotfiles/bashrc ~/.bashrc
-ln -sf ~/src/dotfiles/tmux.conf ~/tmux.conf
+ln -sf ~/src/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/src/dotfiles/aliases.sh ~/.aliases.sh
-
+ln -sf ~/src/dotfiles/config/alacritty.yml ~/.alacritty.yml
+ln -sf ~/src/dotfiles/ripgreprc ~/.ripgreprc
+ln -sf ~/src/dotfiles/vimrc ~/.vimrc
 
 # other stuff to do here
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-ln -sf ~/src/dotfiles/vimrc ~/.vimrc
 
 # then run :PlugInstall in vim
 
