b59ee90fa7b11771701a1d656476e64c3e5309f0
Tony Yunker <tony.yunker@gmail.com>
2023-09-19 13:58:39 -0500

    [nvim] add trouble.nvim and keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -179,6 +179,16 @@ require('lazy').setup({
     end,
   },
 
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  }
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -516,6 +526,28 @@ vim.keymap.set('n', '<leader>ts', ':TestNearest<CR>', { desc = '[T]est Neare[S]t
 vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' })
 
 vim.keymap.set('n', '<leader>so', ':source %<CR>:lua vim.notify("File sourced!")<CR>', { noremap = true, silent = true })
+
+-- TROUBLE
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  { silent = true, noremap = true }
+)
+-- END TROUBLE
+
 
 -- is this a sledgehammer? totally wrong way? it works for meow
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
