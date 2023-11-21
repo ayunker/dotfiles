f8a49169fe7cb1577e04a668bff63a90b9e2aafe
Tony Yunker <tony.yunker@gmail.com>
2023-11-21 10:26:43 -0600

    [alias] update vv git client in nvim

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 #general
 alias v='nvim'
 alias vi='nvim'
-alias vv='nvim -c ":Git"'
+alias vv='nvim -c ":Neogit"'
 alias vimrc='v ~/.vimrc'
 alias viminit='v ~/.config/nvim/init.vim'
 alias bashrc='v ~/.bashrc'
