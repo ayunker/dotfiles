b253c98183b1583ad6dbb1f796eeb8b7c0341180
Tony Yunker <tony.yunker@gmail.com>
2024-06-21 16:12:13 -0500

    [alacritty] migrate config to toml :/

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ ln -sf ~/src/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/src/dotfiles/bashrc ~/.bashrc
 ln -sf ~/src/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/src/dotfiles/aliases.sh ~/.aliases.sh
-ln -sf ~/src/dotfiles/config/alacritty.yml ~/.alacritty.yml
 ln -sf ~/src/dotfiles/ripgreprc ~/.ripgreprc
 ln -sf ~/src/dotfiles/vimrc ~/.vimrc
 ln -sf ~/src/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/src/dotfiles/config/bat/config ~/.config/bat/config
+ln -sf ~/src/dotfiles/wezterm.lua ~/.wezterm.lua
+ln -sf ~/src/dotfiles/config/alacritty ~/.config/alacritty
+# TODO: should I just symlink all of .config???
 
 # other stuff to do here
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
