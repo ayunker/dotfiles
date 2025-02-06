f406f2f24ad0bed1293f07646893c4c9b9820dbf
Tony Yunker <tony.yunker@gmail.com>
2025-02-06 16:44:21 -0600

    [nvim] (plugin refactor) first step get plugins into own dir

---
+++ config/nvim/lua/plugins/init.lua
@@ -0,0 +1,353 @@
+return {
+	-- NOTE: First, some plugins that don't require any configuration
+
+	-- Git related plugins
+	'tpope/vim-fugitive',
+	'tpope/vim-rhubarb',
+
+	-- Detect tabstop and shiftwidth automatically
+	'tpope/vim-sleuth',
+
+	'tpope/vim-surround',
+	'tpope/vim-abolish',
+
+	-- NOTE: This is where your plugins related to LSP can be installed.
+	--  The configuration is done below. Search for lspconfig to find it below.
+	{
+		-- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{
+				'j-hui/fidget.nvim',
+				tag = "legacy",
+				opts = {}
+			},
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+		},
+	},
+
+	{
+		-- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+	},
+
+	-- Useful plugin to show you pending keybinds.
+	{ 'folke/which-key.nvim',  opts = {} },
+	{
+		-- Adds git releated signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+		},
+	},
+
+	{
+		'stevearc/conform.nvim',
+		event = { 'BufWritePre' },
+		keys = {
+			{
+				'<leader>f',
+				function()
+					require('conform').format { async = true, lsp_format = 'fallback' }
+				end,
+				mode = '',
+				desc = '[F]ormat buffer',
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				javascript = { "standardjs" },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+	},
+
+	{
+		'lifepillar/vim-solarized8',
+		branch = 'neovim',
+		config = function()
+			-- vim.o.background = 'dark'
+			-- vim.cmd.colorscheme 'solarized8'
+		end
+	},
+
+	{
+		'maxmx03/solarized.nvim',
+		lazy = false,
+		priority = 1000,
+		---@type solarized.config
+		opts = {},
+		config = function(_, opts)
+			vim.o.termguicolors = true
+			vim.o.background = 'dark'
+			require('solarized').setup(opts)
+			vim.cmd.colorscheme 'solarized'
+		end,
+	},
+
+	{
+		'rebelot/kanagawa.nvim',
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme 'kanagawa-wave'
+		end,
+	},
+
+	{
+		"EdenEast/nightfox.nvim",
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme 'nightfox'
+		end
+	},
+
+	{
+		'folke/tokyonight.nvim',
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme 'tokyonight-night'
+		end,
+	},
+
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		-- config = function()
+		--   vim.cmd.colorscheme 'catppuccin-macchiato'
+		-- end,
+	},
+
+	{
+		"rose-pine/neovim",
+		priority = 1000,
+		name = "rose-pine"
+	},
+
+	{
+		-- Set lualine as statusline
+		'nvim-lualine/lualine.nvim',
+		opts = {
+			options = {
+				theme = 'solarized_light',
+			},
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', { 'diagnostics', symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' } } },
+				lualine_c = { 'filename' },
+				lualine_x = { 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' },
+			}
+		},
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} },
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		'nvim-telescope/telescope.nvim',
+		version = '*',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		opts = {
+			pickers = {
+				buffers = {
+					mappings = {
+						i = {
+							["<c-d>"] = "delete_buffer",
+						},
+						n = {
+							["d"] = "delete_buffer",
+						}
+					}
+				}
+			}
+		}
+	},
+
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+	-- Only load if `make` is available. Make sure you have the system
+	-- requirements installed.
+	{
+		'nvim-telescope/telescope-fzf-native.nvim',
+		-- NOTE: If you are having trouble with this installation,
+		--       refer to the README for telescope-fzf-native for more instructions.
+		build = 'make',
+		cond = function()
+			return vim.fn.executable 'make' == 1
+		end,
+	},
+
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		config = function()
+			pcall(require('nvim-treesitter.install').update { with_sync = true })
+		end,
+	},
+
+	{
+		-- testing is important, yo
+		'vim-test/vim-test',
+		config = function()
+			vim.g['test#strategy'] = "neovim"
+		end,
+	},
+
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xl",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xq",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	{
+		"NeogitOrg/neogit",
+		pin = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			-- "nvim-telescope/telescope.nvim", -- optional
+			-- "sindrets/diffview.nvim",      -- optional
+			-- "ibhagwan/fzf-lua",            -- optional
+		},
+		config = true
+	},
+
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			signs = false,
+			highlight = {
+				multiline = false
+			}
+		}
+	},
+
+	{
+		"ayunker/nvim-chainsaw",
+		branch = "ruby"
+	},
+
+	{
+		"NoahTheDuke/vim-just",
+		ft = { "just" },
+	},
+
+	{
+		'andymass/vim-matchup',
+	},
+
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup {
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<M-h>"] = "actions.select_split",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			}
+
+			-- Open parent directory in current window
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+			-- Open parent directory in floating window
+			-- vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		end,
+	},
+
+	{
+		'tpope/vim-projectionist',
+		config = function()
+			vim.g.projectionist_heuristics = {
+				["package.json"] = {
+					["*.tsx"] = {
+						alternate = {
+							"{dirname}/__tests__/{basename}.test.tsx"
+						}
+					},
+					["*.test.tsx"] = {
+						alternate = {
+							"{dirname}/../{basename}.tsx"
+						}
+					},
+					["*.ts"] = {
+						alternate = {
+							"{dirname}/__tests__/{basename}.test.ts"
+						}
+					},
+					["*.test.ts"] = {
+						alternate = {
+							"{dirname}/../{basename}.ts"
+						}
+					}
+				}
+			}
+		end,
+	},
+
+	'tpope/vim-rails',
+}
